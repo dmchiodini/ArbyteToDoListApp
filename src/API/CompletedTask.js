705e9a28ee90094759c4c29a61453ddbbb511a0e
dmchiodini <g25_diego@hotmail.com>
2020-07-01 13:38:52 -0300

    Alteração na função do checkbox

--- src/API/CompletedTask.js
+++ src/API/CompletedTask.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
-function completarTarefa(token, check, id) {
+function completarTarefa(token, check, description, id) {
 
     return axios.put(`https://arbyte-todo-list-api.herokuapp.com/tasks/${id}`, {
+        description: description,
         completed: check
     },{
         headers: { 'Authorization': `Bearer ${token}` }
